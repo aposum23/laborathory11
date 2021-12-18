5f01a0d9af56b04b778baf0485d9f299f0ee9530
aposum23 <81620056+aposum23@users.noreply.github.com>
2021-12-18 14:09:08 +0300

    Update task.py

--- Programs/task.py
+++ Programs/task.py
@@ -2,6 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
+import pandas as pd
+
+
+def make_table(trains):
+    num_lst = []
+    end_point_lst = []
+    for trn in trains:
+        num_lst.append(trn['num'])
+        end_point_lst.append(trn['name'])
+    data = {'Номер поезда:': num_lst, 'Конечный пункт:': end_point_lst}
+    df = pd.DataFrame(data=data)
+    print(df)
+
 
 
 def add_element():
@@ -15,8 +28,7 @@ def add_element():
     return trains
 
 
-def find_train(trains):
-    num = int(input('Введите номер искомого поезда: '))
+def find_train(trains, num):
     for dcts in trains:
         if dcts['num'] == num:
             print(f'Конечный пункт: {dcts["name"]} \n'
@@ -30,6 +42,9 @@ if __name__ == '__main__':
     flag = True
     trains = []
     while flag:
+        print('\n')
+        make_table(trains)
+        print('\n')
         print('1. Добавить новый поезд')
         print('2. Вывести информацию о поезде')
         print('3.Выход из программы')
@@ -37,6 +52,7 @@ if __name__ == '__main__':
         if com == 1:
             trains.append(add_element())
         elif com == 2:
-            find_train(trains)
+            train_num = input('Введите номер поезда: ')
+            find_train(trains, train_num)
         elif com == 3:
             flag = False
